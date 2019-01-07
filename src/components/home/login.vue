f8c992bc48800d7ce19a443e3b3463e01c81f375
wangcanfeng01 <373811598@qq.com>
2019-01-08 00:14:12 +0800

    Initial commit

---
+++ src/components/home/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="login">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      message: 'my name is wangcanfeng'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
