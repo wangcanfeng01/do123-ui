182581abe57d213db2b540efd10a9a6b7d66e05a
wangcanfeng01 <373811598@qq.com>
2019-03-01 23:57:38 +0800

    修改菜单

--- src/components/admin/admin_menu.vue
+++ src/components/admin/admin_menu.vue
@@ -31,6 +31,16 @@
                 <el-option v-for="(o) in 4" :key="o" :label="o" :value="o"></el-option>
               </el-select>
             </el-form-item>
+            <el-form-item label="菜单类型" label-width="80px">
+              <el-switch v-model="menuForm.menuType" active-color="#13ce66" inactive-color="#ff4949"
+                         active-text="非空菜单" inactive-text="空菜单" active-value="1" inactive-value="0">
+              </el-switch>
+            </el-form-item>
+            <el-form-item label="权限限制" label-width="80px">
+              <el-switch v-model="menuForm.needAuth" active-color="#13ce66" inactive-color="#ff4949"
+                         active-text="是" inactive-text="否" active-value="1" inactive-value="0">
+              </el-switch>
+            </el-form-item>
             <el-form-item label="父菜单" label-width="80px">
               <el-select v-model="menuForm.parent" filterable placeholder="请选择父菜单">
                 <el-option v-for="menu in simpleList" :key="menu.id" :label="menu.menuName"
@@ -49,16 +59,65 @@
         </el-dialog>
         <el-table :data="tableData" style="width: 100%">
           <el-table-column prop="menuName" label="菜单名称" width="120"></el-table-column>
-          <el-table-column prop="menuCode" label="菜单编码" width="140"></el-table-column>
           <el-table-column prop="menuLevel" label="菜单层级" width="80"></el-table-column>
-          <el-table-column prop="creator" label="创建用户" width="120"></el-table-column>
           <el-table-column prop="parentName" label="父菜单" width="120"></el-table-column>
+          <el-table-column prop="needAuth" label="权限要求" width="200">
+            <template slot-scope="scope">
+              <el-button size="mini" :type="scope.row.needAuth === '是' ? 'success' : 'danger'"
+                         @click="changeMenuAuth(scope.row.needAuth,scope.row.id)">{{scope.row.needAuth}}
+              </el-button>
+            </template>
+          </el-table-column>
+          <el-table-column prop="menuType" label="菜单类型" width="200">
+            <template slot-scope="scope">
+              <el-button size="mini" :type="scope.row.menuType === '非空' ? 'success' : 'danger'"
+                         @click="changeMenuType(scope.row.menuType,scope.row.id)">{{scope.row.menuType}}
+              </el-button>
+            </template>
+          </el-table-column>
           <el-table-column prop="menuPath" label="菜单路径" width="200"></el-table-column>
-          <el-table-column prop="createTime" label="创建时间" width="180"></el-table-column>
-          <el-table-column prop="updateTime" label="修改时间" width="180"></el-table-column>
           <el-table-column prop="mark" label="备注"></el-table-column>
           <el-table-column label="操作" width="200">
             <template slot-scope="scope">
+              <el-button size="mini" type="success" @click="showDetailMenu(scope.row)">详情</el-button>
+              <!--菜单详情弹出框-->
+              <el-dialog title="菜单详情" :visible.sync="detailFormVisible" width="400px">
+                <el-form :model="detailForm">
+                  <el-form-item label="菜单名称" label-width="80px">
+                    <el-input v-model="detailForm.menuName" autocomplete="off" disabled></el-input>
+                  </el-form-item>
+                  <el-form-item label="菜单编码" label-width="80px">
+                    <el-input v-model="detailForm.menuCode" autocomplete="off" disabled></el-input>
+                  </el-form-item>
+                  <el-form-item label="菜单层级" label-width="80px">
+                    <el-input v-model="detailForm.menuLevel" autocomplete="off" disabled></el-input>
+                  </el-form-item>
+                  <el-form-item label="创建用户" label-width="80px">
+                    <el-input v-model="detailForm.creator" autocomplete="off" disabled></el-input>
+                  </el-form-item>
+                  <el-form-item label="父菜单" label-width="80px">
+                    <el-input v-model="detailForm.parentName" autocomplete="off" disabled></el-input>
+                  </el-form-item>
+                  <el-form-item label="权限要求" label-width="80px">
+                    <el-input v-model="detailForm.needAuth" autocomplete="off" disabled></el-input>
+                  </el-form-item>
+                  <el-form-item label="菜单类型" label-width="80px">
+                    <el-input v-model="detailForm.menuType" autocomplete="off" disabled></el-input>
+                  </el-form-item>
+                  <el-form-item label="菜单路径" label-width="80px">
+                    <el-input v-model="detailForm.menuPath" autocomplete="off" disabled></el-input>
+                  </el-form-item>
+                  <el-form-item label="创建时间" label-width="80px">
+                    <el-input v-model="detailForm.createTime" autocomplete="off" disabled></el-input>
+                  </el-form-item>
+                  <el-form-item label="修改时间" label-width="80px">
+                    <el-input v-model="detailForm.updateTime" autocomplete="off" disabled></el-input>
+                  </el-form-item>
+                  <el-form-item label="备注" label-width="80px">
+                    <el-input v-model="detailForm.mark" autocomplete="off" disabled></el-input>
+                  </el-form-item>
+                </el-form>
+              </el-dialog>
               <el-button size="mini" type="warning" @click="openUpdateDialog(scope.row)">修改</el-button>
               <!--编辑菜单弹出框-->
               <el-dialog title="编辑菜单" :visible.sync="modifyFormVisible" width="400px">
@@ -126,6 +185,8 @@ export default {
         creator: '3',
         parentName: '测试',
         menuPath: '4',
+        menuType: '',
+        needAuth: '',
         createTime: '5',
         updateTime: '6',
         mark: ''
@@ -135,6 +196,7 @@ export default {
       pageSize: 10,
       addFormVisible: false,
       modifyFormVisible: false,
+      detailFormVisible: false,
       menuForm: {
         id: null,
         name: '',
@@ -142,7 +204,9 @@ export default {
         level: null,
         path: '',
         mark: '',
-        parent: null
+        parent: null,
+        needAuth: 0,
+        menuType: 0
       },
       simpleList: [{
         id: 1,
@@ -152,7 +216,20 @@ export default {
         id: 2,
         menuName: '管理中心',
         menuPath: '/admin/center'
-      }]
+      }],
+      detailForm: {
+        menuName: '1',
+        menuCode: '菜单编码',
+        menuLevel: 2,
+        creator: '3',
+        parentName: '测试',
+        menuPath: '4',
+        menuType: '',
+        needAuth: '',
+        createTime: '5',
+        updateTime: '6',
+        mark: ''
+      }
     }
   },
   methods: {
@@ -164,6 +241,8 @@ export default {
       this.menuForm.level = null
       this.menuForm.mark = ''
       this.menuForm.parent = null
+      this.menuForm.needAuth = 0
+      this.menuForm.menuType = 0
     },
     openUpdateDialog (menu) {
       this.modifyFormVisible = true
@@ -174,6 +253,20 @@ export default {
       this.menuForm.level = menu.menuLevel
       this.menuForm.mark = menu.mark
       this.menuForm.parent = menu.parentNode
+    },
+    showDetailMenu (menu) {
+      this.detailForm.menuName = menu.menuName
+      this.detailForm.menuCode = menu.menuCode
+      this.detailForm.menuLevel = menu.menuLevel
+      this.detailForm.creator = menu.creator
+      this.detailForm.menuPath = menu.menuPath
+      this.detailForm.menuType = menu.menuType
+      this.detailForm.needAuth = menu.needAuth
+      this.detailForm.createTime = menu.createTime
+      this.detailForm.updateTime = menu.updateTime
+      this.detailForm.parentName = menu.parentName
+      this.detailForm.mark = menu.mark
+      this.detailFormVisible = true
     },
     handleAddMenu () {
       this.addFormVisible = false
@@ -258,6 +351,52 @@ export default {
           }
         } else {
           this.$message.error('菜单编辑异常')
+        }
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    changeMenuAuth (needAuth, id) {
+      let menuAuth
+      if (needAuth === '是') {
+        menuAuth = 0
+      } else {
+        menuAuth = 1
+      }
+      this.$http.put('/ui/menu/auth?id=' + id + '&auth=' + menuAuth).then(response => {
+        if (response && response.data) {
+          if (response.data.code === '0') {
+            // 修改菜单后刷新列表
+            this.menuList(this.pageSize, this.currentPage)
+            this.$message.success('权限修改成功')
+          } else {
+            this.$message.error(response.data.msg)
+          }
+        } else {
+          this.$message.error('权限修改异常')
+        }
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    changeMenuType (type, id) {
+      let menuType
+      if (type === '非空') {
+        menuType = 0
+      } else {
+        menuType = 1
+      }
+      this.$http.put('/ui/menu/type?id=' + id + '&type=' + menuType).then(response => {
+        if (response && response.data) {
+          if (response.data.code === '0') {
+            // 修改菜单后刷新列表
+            this.menuList(this.pageSize, this.currentPage)
+            this.$message.success('菜单类型修改成功')
+          } else {
+            this.$message.error(response.data.msg)
+          }
+        } else {
+          this.$message.error('菜单类型修改异常')
         }
       }).catch(error => {
         console.log(error)
